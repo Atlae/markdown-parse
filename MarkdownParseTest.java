710c27c5a49451b295d792b420c0653813674dc7
Gary <atlae.ns@gmail.com>
2022-01-26 19:56:46 -0800

    Created tester

---
+++ MarkdownParseTest.java
@@ -0,0 +1,13 @@
+// compile and run commands
+// javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java
+// java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class MarkdownParseTest {
+    @Test // declares a test method
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
+}
