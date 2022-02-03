315db4bc3d4cb7e9bbacc0eb573b6ffd0d82f898
Gary <atlae.ns@gmail.com>
2022-02-02 20:24:05 -0800

    failure inducing

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -15,35 +15,44 @@ public class MarkdownParseTest {
     }
 
 @Test 
- public void Testgetlinks()throws IOException{
+ public void testGetLinks()throws IOException{
 
     Path fileName = Path.of("test-file.md");
 	String contents = Files.readString(fileName);
     ArrayList<String> links = MarkdownParse.getLinks(contents);
-    // assertEquals(List.of("https://something.com","some-page.html"),links);
+    assertEquals(List.of("https://something.com","some-page.html"),links);
  }
  @Test 
- public void Testgetlink2()throws IOException{
+ public void testGetLinks2()throws IOException{
 
     Path fileName = Path.of("test-file2.md");
 	String contents = Files.readString(fileName);
     ArrayList<String> links = MarkdownParse.getLinks(contents);
-    // assertEquals(List.of("something.in"),links);
+    assertEquals(List.of("something.in"),links);
  }
  @Test 
- public void Testgetlink3()throws IOException{
+ public void testGetLinks3()throws IOException{
 
     Path fileName = Path.of("test-file3.md");
 	String contents = Files.readString(fileName);
     ArrayList<String> links = MarkdownParse.getLinks(contents);
-    // assertEquals(List.of("rocknroll.com","wowcserocks.com","profpolitzisprettychill.com","gotritons.edu"),links);
+    assertEquals(List.of("rocknroll.com","wowcserocks.com","profpolitzisprettychill.com","gotritons.edu"),links);
  }
  @Test 
- public void Testgetlink4()throws IOException{
+ public void testGetLinks4()throws IOException{
 
     Path fileName = Path.of("test-file4.md");
 	String contents = Files.readString(fileName);
     ArrayList<String> links = MarkdownParse.getLinks(contents);
-    // assertEquals(List.of(),links);
+    assertEquals(List.of(),links);
+ }
+
+ @Test
+ public void testGetLinks5()throws IOException{
+
+    Path fileName = Path.of("test-file5.md");
+	String contents = Files.readString(fileName);
+    ArrayList<String> links = MarkdownParse.getLinks(contents);
+    assertEquals(List.of(),links);
  }
 }
